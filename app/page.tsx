9f95e4dfa4d4dfe9ae0945f1ea92a5f6b308fade
Jeserry Radcliffe <radcliffejeserry@gmail.com>
2026-04-11 15:59:40 +0800

    changes some paddings and site descriptions, also added image in ojt and added more projects links

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ThemeProvider } from "next-themes";
@@ -16,6 +17,9 @@ import { MdOutlineWorkOutline } from "react-icons/md";
 import { FaChevronCircleUp } from "react-icons/fa";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false); //For Image
+  const [isTextOpen, setIsTextOpen] = useState(false); // For the Text
+
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black w-full">
       <main
@@ -28,11 +32,20 @@ export default function Home() {
           className="text-5xl font-semibold my-15 bg-cover bg-center lg:px-5
         lg:text-7xl lg:mt-20"
         >
-          Building bridges between design and code
+          Building bridges between{" "}
+          <span className="text-pink-500 animate-[pulse_5s_infinite]">
+            design
+          </span>{" "}
+          and <span className="text-blue-500 animate-pulse">code</span>
         </p>
         <p className="lg:text-xl  lg:px-5">
-          I'm Jeserry, a design engineer, programmer, where I craft intuitive
-          user experiences. After hours, I build my own projects.
+          {/* I'm Jeserry, a design engineer, programmer, where I craft intuitive
+          user experiences. After hours, I build my own projects. */}
+          I'm <span className="text-amber-400 font-bold">Jeserry</span> a
+          Computer Science graduate an aspiring programmer. I enjoy turning
+          ideas into working lines of code, building a clean, simple websites
+          using modern tools. I'm open for many opportunities to start my tech
+          career.
         </p>
         <a
           href="#about"
@@ -58,7 +71,6 @@ export default function Home() {
             &nbsp;&nbsp; Work Experiences
           </Button>
         </a>
-
         <div
           className="w-full flex flex-col justify-between mt-40 mb-20 scroll-mt-20
           lg:flex-row"
@@ -68,7 +80,7 @@ export default function Home() {
             <img
               src="/res/photo-jeserry.jpg"
               alt="jeserry.jpg"
-              className="w-45 h-45 rounded-full object-cover border border-gray-200 lg:w-56 lg:h-56 lg:items-center lg:mt-0"
+              className="w-45 h-45 rounded-full object-cover  border-amber-500  border-2 lg:w-56 lg:h-56 lg:items-center lg:mt-0"
             />
             <div className="flex justify-center w-full p-1 items-center mt-3">
               <TiWorldOutline className="w-6 h-6 text-center text-blue-500" />
@@ -89,7 +101,7 @@ export default function Home() {
               <a href="#contact">
                 <Button
                   variant="outline"
-                  className="rounded-full p-5 mt-10 mb-7
+                  className="rounded-full p-5 mt-10 mb-7 
                   "
                 >
                   <MdEmail
@@ -113,7 +125,7 @@ export default function Home() {
               </p>
             </div>
 
-            <div className="mt-7 flex justify-between text-center w-[70%] mx-auto lg:w-[50%] lg:mx-0">
+            <div className="mt-7 flex justify-between text-center w-[90%] mx-auto lg:w-[50%] lg:mx-0">
               <div className="inline-flex items-center justify-center w-10 h-10 rounded-full border border-gray-400 p-1 ">
                 <a href="https://github.com/Jeserry-Radcliffe">
                   <FaGithub style={{ width: "25px", height: "25px" }} />
@@ -138,10 +150,12 @@ export default function Home() {
 
             <div className="mt-15 flex justify-between lg:text-xl">
               <p>
-                Jeserry is a Philippine-based design engineer with a passion for
-                transforming complex challenges into simple, elegant design
-                solutions. His work spans digital interfaces, interactive
-                experiences, and the convergence of design and technology.
+                <strong>Jeserry</strong> is an URS Alumni and Cum Laude
+                graduate. An aspiring programmer with a passion in transforming
+                ideas and challenges into clean, simple yet elegant and working
+                applications. During after hours, he does some personal
+                projects, walking, cycling and attending tech seminars or
+                webinars.
               </p>
             </div>
 
@@ -150,7 +164,7 @@ export default function Home() {
                 <h1 className="text-3xl">Work Experience</h1>
               </div>
 
-              <div className="flex flex-col w-full my-2">
+              <div className="flex flex-col w-full my-5">
                 <div className="flex justify-between items-center">
                   <p className="text-2xl font-bold">Pru Life UK</p>
                   <p className="text-gray-400">2025</p>
@@ -182,7 +196,7 @@ export default function Home() {
                 </ul>
               </div>
 
-              <div className="flex flex-col w-full my-2 ">
+              <div className="flex flex-col w-full my-5 ">
                 <div className="flex justify-between items-center">
                   <p className="text-2xl font-bold">Pru Life UK</p>
                   <p className="text-gray-400">2025</p>
@@ -202,14 +216,47 @@ export default function Home() {
                     {" "}
                     Helping staff with tech questions &#40;IT SUPPORT&#41;
                   </li>
+                  <br />
                   <li>Provides basic troubleshooting &#40;Software&#41;</li>
                 </ul>
+
+                <div>
+                  <button
+                    onClick={() => setIsOpen(true)}
+                    className="text-amber-500 hover:underline"
+                  >
+                    See Photo
+                  </button>
+
+                  <div className="">
+                    {isOpen && (
+                      <div className="fixed inset-0 z-50 flex flex-col items-center justify-center pb-20 px-3 bg-black/90">
+                        <button
+                          onClick={() => setIsOpen(false)}
+                          className=" text-white text-4xl m-3 font-light hover:scale-110 transition-transform lg:mt-10 lg:text-5xl"
+                        >
+                          &times;
+                        </button>
+                        <div className="flex flex-col items-center">
+                          <img
+                            className="rounded-xl md: lg:w-[60%]"
+                            src="res/ojt-prulife.jpeg"
+                            alt="ojt-prulife.jpeg"
+                          />
+                          <p className="py-5 text-white">
+                            My OJT Photo - March 2025
+                          </p>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
               </div>
 
-              <div className="flex flex-col w-full my-2">
+              <div className="flex flex-col w-full my-5">
                 <div className="flex justify-between items-center">
                   <p className="text-2xl font-bold">URS Morong</p>
-                  <p className="text-gray-400">2023 - 2024</p>
+                  <p className="text-gray-400">2024</p>
                 </div>
                 <span className="text-blue-400 text-sm">
                   Budget Student Assistant
@@ -402,6 +449,32 @@ export default function Home() {
               </div>
             </div>
 
+            <div>
+              <button onClick={() => setIsTextOpen(true)} className="underline">
+                <i>More of My Projects</i>
+              </button>
+
+              <div className="">
+                {isTextOpen && (
+                  <div className="fixed inset-0 z-50 flex flex-col items-center justify-center pb-20 px-3 bg-black/90">
+                    <button
+                      onClick={() => setIsTextOpen(false)}
+                      className=" text-white text-4xl m-3 font-light hover:scale-110 transition-transform"
+                    >
+                      &times;
+                    </button>
+                    <div>
+                      <p className="w-[80%] mx-auto py-5">Sorry this page is currently in progress. Thank you.</p>
+                    </div>
+                  </div>
+                )}
+              </div>
+
+              {/* <a href="" className="underline">
+                <i>More of My Projects</i>
+              </a> */}
+            </div>
+
             <div className="my-15 scroll-mt-12 w-full" id="contact">
               <div className="w-full flex my-2 ">
                 <h1 className="text-3xl">Contact</h1>
@@ -412,7 +485,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-
         <footer className="text-center flex flex-col items-center justify-center w-full mx-auto">
           <p className="text-sm mb-10 lg:text-lg">
             © Mark Jeserry Sicat - All Rights Reserved 2026
@@ -433,7 +505,6 @@ export default function Home() {
             </div>
           </div>
         </footer>
-
         <div className="fixed bottom-5 right-6">
           <a href="#top" className="">
             <Button
@@ -445,10 +516,8 @@ export default function Home() {
             </Button>
           </a>
         </div>
-
         <br />
         <br />
-
         <br />
         <br />
       </main>
